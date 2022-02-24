269f2a8436d29e6a67e4231b41e63a6e37b894fa
K1f1r1k <ennikiforova@edu.hse.ru>
2022-02-24 22:15:14 +0300

    rabotaet!!!!!!

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,1 +1,9 @@
 //write here your js
+$(function() {
+	$(".cnst").draggable();
+});
+$(document).ready(function(){
+	$(".cnst").mouseup(function(){
+	$(this).css({ 'animation' : 'move1'})
+	});
+});
