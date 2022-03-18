6ce6e9fe560eda56719cc0451b09a183daa7dcd6
K1f1r1k <ennikiforova@edu.hse.ru>
2022-03-18 11:48:04 +0300

    tut vse horosho

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -12,16 +12,16 @@ $(document).ready(function(){
 });
 $(document).ready(function(){
 	$(".arrow").click(function(){
-		if($(".clothes").css({ 'clip-path' : 'url(#myClip)'})){
-	$(".clothes").css({ 'clip-path' : 'url(#clip2)'})}
-	else{$(".clothes").css({ 'clip-path' : 'url(#clip3)'})}
+		if($(".clothes").css({ 'clip-path' : 'url(#platye1)'})){
+	$(".clothes").css({ 'clip-path' : 'url(#ubka1)'})}
 	});
 });
 
 $(document).ready(function(){
 	$(".scissors").click(function(){
-	$("path").attr('d', "M0.321663 0L0 0.178862L0.06 0.1875L0.0506262 0.231419H0.105455L0.0997114 0.282376H0.148796V0.333333L0.321663 0.221545L0 1L0.12 0.952703L0.189091 1L0.274545 0.952703L0.344545 1L0.416364 0.952703L0.5 1L0.583636 0.952703L0.655455 1L0.725455 0.952703L0.810909 1L0.88 0.952703L1 1L0.678337 0.221545L0.851204 0.333333V0.282376H0.900289L0.894545 0.231419H0.949374L0.94 0.1875L1 0.178862L0.678337 0L0.5 0.124155L0.321663 0Z")
-	});
+   if($(".clothes").css({ 'clip-path' : 'url(#platye1)'})){
+$(".clothes").css({ 'clip-path' : 'url(#platye2)'})}
+});
 });
 
 $(document).ready(function(){
